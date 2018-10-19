5f71b651c0d3ee6825b597c8b7ebd10f8180b909
Franz Angelo Ygar <franzygar@telehealth.ph>
2018-10-19 13:29:36 +0800

    trial commit then push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel this is from development branch
+                    Laravel this is from development branch with commit
                 </div>
 
                 <div class="links">
