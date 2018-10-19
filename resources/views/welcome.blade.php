25c75d005ee22ce5c656a46a53c694a1fddd2cec
Franz Angelo Ygar <franzygar@telehealth.ph>
2018-10-19 12:59:32 +0800

    test commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel this is from development branch
                 </div>
 
                 <div class="links">
